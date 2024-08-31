70a3bcca9e914c678e2ee9da964591b4e585be0a
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2024-09-01 04:49:25 +0600

    Add files via upload

---
+++ FindRepeatElementIndex.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int N=2e6+2;
+    int idx[N];
+    for(int i=0;i<N;i++)
+    {
+        idx[i]=-1;
+    }
+    int minidx=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(idx[arr[i]]!=-1)
+        {
+            minidx=min(minidx,idx[arr[i]]);
+        }
+        else
+        {
+            idx[arr[i]]=i;
+        }
+    }
+    if(minidx==INT_MAX)
+    {
+        cout<<"-1";
+    }
+    else
+    {
+        cout<<minidx+1<<endl;
+    }
+    return 0;
+}
